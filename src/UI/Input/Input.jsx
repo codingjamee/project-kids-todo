4ba9b680384725cf81e5eb9c529b2a73a066f6eb
jamee <codingjamee@gmail.com>
2022-12-14 22:13:02 +0900

    로그인창 css디자인, 버튼에 react아이콘넣기

--- src/UI/Input/Input.jsx
+++ src/UI/Input/Input.jsx
@@ -1,12 +1,15 @@
+import classes from "./Input.module.css";
+
 const Input = (props) => {
   return (
-    <div>
+    <div className={classes.control}>
       <label htmlFor={props.id}>{props.label}</label>
       <input
         type={props.type}
         id={props.id}
         value={props.value}
         onChange={props.onChange}
+        placeholder={props.placeholder}
       />
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import Card from "../UI/Card";
 import Input from "../UI/Input/Input";
+import classes from "./Login.module.css";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { BsFacebook } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+
+import { IconContext } from "react-icons";
 
 const Login = () => {
   const [idState, setIdState] = useState("");
@@ -19,26 +25,51 @@ const Login = () => {
     setPwState(e.target.value);
   };
   return (
-    <Card>
-      <form action="submit" onSubmit={submitHandler}>
-        <Input
-          type="email"
-          id="email"
-          label="아이디"
-          onChange={idChangeHandler}
-          value={idState}
-        />
-        <Input
-          type="password"
-          id="password"
-          label="비밀번호"
-          onChange={pwChangeHandler}
-          value={pwState}
-        />
-        <button type="submit">로그인</button>
+    <>
+      <Card className={classes.login}>
+        <form action="submit" onSubmit={submitHandler}>
+          <Input
+            type="email"
+            id="email"
+            label="아이디(이메일)"
+            onChange={idChangeHandler}
+            value={idState}
+            placeholder="아이디를 입력해주세요"
+          />
+          <Input
+            type="password"
+            id="password"
+            label="비밀번호"
+            onChange={pwChangeHandler}
+            value={pwState}
+            placeholder="비밀번호를 입력해주세요"
+          />
+          <div className={classes.actions}>
+            <button type="submit">로그인</button>
+          </div>
+        </form>
+      </Card>
+      <div className={classes.signup}>
+        <button>아이디찾기</button>
+        <button>비밀번호 찾기</button>
         <button>회원가입</button>
-      </form>
-    </Card>
+      </div>
+
+      <div className={classes.snslog}>
+        <p>--- SNS 계정으로 로그인 ---</p>
+        <div className={classes.signup}>
+          <button className={classes.kakao}>
+            <RiKakaoTalkFill />
+          </button>
+          <button className={classes.facebook}>
+            <BsFacebook />
+          </button>
+          <button className={classes.gmail}>
+            <SiGmail />
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
